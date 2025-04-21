7935df4e73dfaaf203086dc2d07d03f817cbbb0f
veikk <valttejo@metropolia.fi>
2025-04-21 23:50:55 +0300

    React, but bello

--- my-app/src/views/Home.jsx
+++ my-app/src/views/Home.jsx
@@ -2,8 +2,7 @@ import {useEffect, useState} from 'react';
 import MediaRow from '../components/MediaRow';
 import SingleView from '../components/SingleView';
 import {fetchData} from '../utils/fetchData';
-
-
+import {uniqBy} from 'lodash';
 
 const Home = () => {
   const [mediaArray, setMediaArray] = useState([]);
@@ -11,10 +10,38 @@ const Home = () => {
 
   const getMedia = async () => {
     try {
-      const json = await fetchData('test.json');
-      setMediaArray(json);
+      const mediaData = await fetchData(
+        import.meta.env.VITE_MEDIA_API + '/media',
+      );
+
+      const uniqueUserIds = uniqBy(mediaData, 'user_id');
+
+      console.log('uniqueUserIds', uniqueUserIds);
+
+      const authApiUrl = import.meta.env.VITE_AUTH_API;
+
+      const userData = await Promise.all(
+        uniqueUserIds.map(
+          async (item) =>
+            await fetchData(`${authApiUrl}/users/${item.user_id}`),
+        ),
+      );
+
+      console.log('userData', userData);
+
+      const userMap = userData.reduce((map, {user_id, username}) => {
+        map[user_id] = username;
+        return map;
+      }, {});
+
+      const newData = mediaData.map((item) => ({
+        ...item,
+        username: userMap[item.user_id],
+      }));
+
+      setMediaArray(newData);
     } catch (error) {
-      console.error('Error fetching media:', error);
+      console.error('error', error);
     }
   };
 
@@ -23,7 +50,7 @@ const Home = () => {
   }, []);
 
   console.log(mediaArray);
-  console.log('selectedItem', selectedItem);
+
   return (
     <>
       <h2>My Media</h2>
@@ -33,6 +60,7 @@ const Home = () => {
           <th>Thumbnail</th>
           <th>Title</th>
           <th>Description</th>
+          <th>Owner</th>
           <th>Created</th>
           <th>Size</th>
           <th>Type</th>
