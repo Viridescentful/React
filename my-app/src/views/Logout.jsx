e1980252be1e502a2a7a8ab2c4a10faa1b5b0862
veikk <valttejo@metropolia.fi>
2025-04-28 01:34:19 +0300

    Added Context to deliver date about the user's current login status, which is used to showcase certain components and prevent user from accessing some when logged out

--- my-app/src/views/Logout.jsx
+++ my-app/src/views/Logout.jsx
@@ -1,5 +1,8 @@
-const Logout = () => {
+import {useUserContext} from '../hooks/contextHooks';
 
+const Logout = () => {
+  const {handleLogout} = useUserContext();
+  handleLogout();
   return <p>Logout page here</p>;
 };
 
